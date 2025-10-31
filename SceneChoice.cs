fce4576c2b7b848680e2e68d2b8664f05ea9aa4a
ElminP <elpit25@student.sdu.dk>
2025-10-31 11:58:46 +0100

    Added: SceneChoice class

---
+++ SceneChoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs
+{
+    public class SceneChoice
+    {
+        public string? Description { get; set; }
+        public int SceneId { get; set; }
+        public Scene? SceneObj { get; set; }
+    }
+}
