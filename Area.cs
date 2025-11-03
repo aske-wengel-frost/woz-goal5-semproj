cef5f3943f26867f4e358a2c9465a8e9b567e959
aske-wengel-frost <askefrost26@gmail.com>
2025-11-03 18:14:49 +0100

    Added: Area class with respect to design.

---
+++ Area.cs
@@ -0,0 +1,11 @@
+public class Area
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    
+    public Area(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+}
