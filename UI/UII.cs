282ce31f89a8f582b00b73e21acc63889ba51273
aske-wengel-frost <askefrost26@gmail.com>
2025-11-03 23:04:23 +0100

    Added interface for UI.

---
+++ UI/UII.cs
@@ -0,0 +1,13 @@
+using cs;
+
+namespace cs.UI
+{
+    public interface UII
+    {
+        public void DrawScene(Scene scene, StoryHandler storyHandler);
+        public string GetUserInput(string prompt = ">> ");
+        public void ClearUI();
+        public void DrawError(string err);
+    }
+
+}
---
+++ UI/UITerm.cs
@@ -0,0 +1,42 @@
+using cs;
+
+namespace cs.UI
+{
+    public class UITerm : UII
+    {
+        public UITerm()
+        { }
+
+        public void ClearUI()
+        {
+            Console.Clear();
+        }
+
+        public void DrawError(string err)
+        {
+            Console.WriteLine($"Error: {err}");
+        }
+
+        public string GetUserInput(string prompt = ">> ")
+        {
+            Console.Write(prompt);
+            string? usrInp = Console.ReadLine();
+            return usrInp != null ? usrInp : "";
+
+        }
+
+        public void DrawScene(Scene scene, StoryHandler storyHandler)
+        {
+            Console.WriteLine($"Scene: {scene.Name} - You are in Area: {scene.Area.Name}");
+            Console.WriteLine();
+            Console.WriteLine("=================================================================");
+            Console.WriteLine(scene.DialogueText);
+
+            // Draw all current choices.
+            Console.WriteLine("Your current opts are: ");
+            foreach (SceneChoice sceneChoice in scene.Choices)
+            { Console.WriteLine($" -> {sceneChoice.Description} : [{sceneChoice.SceneId}]"); }
+        }
+    }
+
+}
