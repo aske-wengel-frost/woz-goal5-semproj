3dd84f27b32ef76d57e74f3f25ae4cf0b9f47cfa
ElminP <elpit25@student.sdu.dk>
2025-11-03 22:04:06 +0100

    Added Function: Inventory.cs & Item.cs (With Comments)

--- Inventory.cs
+++ Inventory.cs
@@ -2,4 +2,82 @@
  */
 
 using System;
+using System.Collections.Generic;
 
+class Inventory
+{ 
+    private List<Item> items;
+
+    // Constructor for inventory initialization
+    public Inventory()
+    {
+        items = new List<Item>();
+    }
+
+    // Method to add an item to the inventory
+    public void AddItem(Item item)
+    {
+        if (item != null)
+        {
+            items.Add(item);
+            Console.WriteLine($"Item: {item.Name} Added to Inventory"); //Debugger message to check
+        }
+    }
+
+    // Method to remove an item from the inventory
+    public void RemoveItem(Item item)
+    {
+        // Check control for item existence (Remove if-statement later if needed)
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Console.WriteLine($"Item: {item.Name} Removed from Inventory"); // Debugger message to check
+        }
+        else
+        {
+            Console.WriteLine($"Item not found in inventory."); //Also a debugger message
+        }
+    }
+
+    /// <summary>
+    /// Method to get an item by its ID
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <returns></returns>
+    public Item GetItem(int ID)
+    { 
+        /// Foreach loop that check if the ID matches what the player is looking for
+        foreach (Item item in items)
+        {
+            if (item.ID == ID)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+
+    public void SetItem(int ID, Item item)
+    {
+        item.ID = ID;
+        items.Add(item);
+        Console.WriteLine($"Item: {item.Name}. Placed in the scene at position {ID}"); // Print debugger message
+    }
+
+    public void ShowInventory()
+    {
+        if (items.Count == 0)
+        {
+            Console.WriteLine("Inventory is empty.");
+            return;
+        }
+        else
+        {
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
--- Item.cs
+++ Item.cs
@@ -2,4 +2,26 @@
  */
 
 using System;
+using System.Collections.Generic;
 
+class Item
+{
+    // Just simple properties for the Item class that use Getters and Setters 
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    // Constructor for the Item class
+    public Item(int ID, string name, string description)
+    {
+        this.ID = ID;
+        this.Name = name;
+        this.Description = description;
+    }
+
+    // A way to view the given item details
+    public override string ToString()
+    {
+        return $"Item ID: {ID}, Name: {Name}, Description: {Description}";
+    }
+}
