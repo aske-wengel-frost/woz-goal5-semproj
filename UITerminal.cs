f4717c50014f6357df160da7a0ec28a63ba4d4b6
Lukas Friis <lukasfriis2003@hotmail.com>
2025-11-04 10:19:56 +0100

    Added: UITermninal class

---
+++ UITerminal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs
+{
+    internal class UITerminal : IUIHandler
+    {
+        public void DrawScene(Scene scene)
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                Console.Write("#");
+            }
+        }
+
+        public string GetUserInput(String input)
+        {
+            return input;
+        }
+
+        public void ClearScreen()
+        {
+            Console.Clear();
+        }
+
+        public void DrawError()
+        {
+            Console.WriteLine("An error has occurred. The game will restart.");
+        }
+    }
+}
