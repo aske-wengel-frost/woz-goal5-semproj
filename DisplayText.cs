3c02555e9d317fcca26ad60d438ca9ec10ab8551
magsemus <magsemus@gmail.com>
2025-11-06 16:03:30 +0100

    Modified the DisplayText.cs to be simplified with better names for variables. Also added comments to help understand the code.

--- DisplayText.cs
+++ DisplayText.cs
@@ -1,251 +1,83 @@
-﻿namespace cs
+﻿using System;
+using System.Threading.Tasks;
+
+namespace cs
 {
-
-    using System;
-    using System.Data;
-    using System.Diagnostics;
-    using System.Formats.Asn1;
-    using System.Net.NetworkInformation;
-    using System.Runtime.InteropServices;
-    using System.Runtime.InteropServices.Marshalling;
-    using System.Text;
-    using System.Threading;
-
-    using static System.Runtime.InteropServices.JavaScript.JSType;
-
-    class Program
+    public static class textDisplay
     {
-        private static readonly object consoleLock = new();
-        static bool stopAnimation = false;
         static Dictionary<Style, int> lowertypeDictionary = new Dictionary<Style, int> { { Style.Normal, 0 }, { Style.Bold, 0x1D41A }, { Style.Italic, 0x1D44E }, { Style.BoldItalic, 0x1D482 }, { Style.Script, 0x1D4B6 }, { Style.BoldScript, 0x1D4EA }, { Style.Fraktur, 0x1D51E }, { Style.DoubleStruck, 0x1D552 }, { Style.BoldFraktur, 0x1D586 }, { Style.SansSerif, 0x1D5BA }, { Style.BoldSansSerif, 0x1D5EE }, { Style.ItalicSansSerif, 0x1D622 }, { Style.BoldItalicSansSerif, 0x1D656 }, { Style.Monospace, 0x1D68A } };
         static Dictionary<Style, int> uppertypeDictionary = new Dictionary<Style, int> { { Style.Normal, 0 }, { Style.Bold, 0x1D400 }, { Style.Italic, 0x1D434 }, { Style.BoldItalic, 0x1D468 }, { Style.Script, 0x1D49C }, { Style.BoldScript, 0x1D4D0 }, { Style.Fraktur, 0x1D504 }, { Style.DoubleStruck, 0x1D538 }, { Style.BoldFraktur, 0x1D56C }, { Style.SansSerif, 0x1D5A0 }, { Style.BoldSansSerif, 0x1D5D4 }, { Style.ItalicSansSerif, 0x1D608 }, { Style.BoldItalicSansSerif, 0x1D63C }, { Style.Monospace, 0x1D670 } };
-        static Style[] array = { Style.Normal, Style.BoldItalic, Style.Bold, Style.Italic, Style.BoldItalic, Style.Script, Style.BoldScript, Style.Fraktur, Style.DoubleStruck, Style.BoldFraktur, Style.SansSerif, Style.BoldSansSerif, Style.ItalicSansSerif, Style.BoldItalicSansSerif, Style.Monospace };
-        static List<string> TextList = new List<string>();
-        static List<int> LeftList = new List<int>();
-        static List<int> TopList = new List<int>();
-        static List<ConsoleColor> ColorList = new List<ConsoleColor>();
-        static List<bool> Animation1List = new List<bool>();
-        static List<bool> Animation2List = new List<bool>();
-        static List<int> AnsiList = new List<int>();
-        static List<int> NumberList = new List<int>();
-        static Random random = new Random();
-        static int number = 0;
-        static int count = 0;
-        static int sAnsi = 10;
-        static int left;
-        static int top;
-        static bool thread1;
-        static bool thread2;
+        public enum Style { Normal, Bold, Italic, BoldItalic, Script, BoldScript, Fraktur, DoubleStruck, BoldFraktur, SansSerif, BoldSansSerif, ItalicSansSerif, BoldItalicSansSerif, Monospace }
+        // The initialization of the Enum types of the text styles and the Dictionaries that hold the corresponding unicode hexadecimal for each taext styles.  
 
-        enum Style { Normal, Bold, Italic, BoldItalic, Script, BoldScript, Fraktur, DoubleStruck, BoldFraktur, SansSerif, BoldSansSerif, ItalicSansSerif, BoldItalicSansSerif, Monospace }
+        /// <summary>
+        /// A class that is capable of displaying text in the terminal in a more Storywise fashion
+        /// </summary>
+        /// <param name="text">The text that to be displayed onto the terminal</param>
+        /// <param name="newLine">A flag that determines if the next text should be displayed on the same line or a new</param>
+        /// <param name="charDelay">The delay between the display of each character</param>
+        /// <param name="punctDelay">The multiplier that multiplies the charDelay when a punctuation marks is met</param>
+        /// <param name="color">An enum type that determines the color of the text</param>
+        /// <param name="backgroundColor">An enum type that determines the background color of the text</param>
+        /// <param name="ANSI">An integer that determines how the text should be displayed in ANSI espace code</param>
+        /// <param name="TextStyle">An enum type that determines the text style of the text to be displayed on the terminal</param>
 
-        static void print(string text, bool enter = true, int charDelay = 100, int punctDelay = 1, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, int Ansi = 10, bool Animation1 = false, Style style = Style.Normal, bool Animation2 = false)
+        public static void Display(string text, bool newLine = true, int charDelay = 100, int punctDelay = 1, ConsoleColor color = ConsoleColor.White, ConsoleColor backgroundColor = ConsoleColor.Black, int ANSI = 10, Style TextStyle = Style.Normal)
         {
+            // The "Console.OutputEncoding = System.Text.Encoding.UTF8;" basically makes the Consoles STDIN able to interpret the unicode of the text styles.
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.InputEncoding = System.Text.Encoding.UTF8;
-            Console.CursorVisible = false;
-            count = 0;
-            int sLeft = Console.CursorLeft;
-            int sTop = Console.CursorTop;
-            int multiplier = 1;
-            number = Array.IndexOf(array, style);
-            sAnsi = Ansi;
-            Console.BackgroundColor = backgroundColor;
-            foreach (char c in text)
+
+            //Basically a for loop that iterates between eách character in the text 
+            foreach (char character in text)
             {
-                do
+                // Code that tells the Console class the color and background color of the text, that was given by the parameter of the method
+                Console.ForegroundColor = color;
+                Console.BackgroundColor = backgroundColor;
+
+                //An if statement that checks if the given Enum text style type is present in the Dictionary and an else statement that will display an error message.
+                if (lowertypeDictionary.ContainsKey(TextStyle) || uppertypeDictionary.ContainsKey(TextStyle))
                 {
-                    if (!thread2)
+                    //An if statement that check if the character is a character of the lowercase english alphabet and makes sure to skip if the text style is Normal.
+                    if (character >= 'a' && character <= 'z' && TextStyle != Style.Normal)
                     {
-                        thread1 = true;
-                        if (lowertypeDictionary.ContainsKey(style) || uppertypeDictionary.ContainsKey(style) || Animation2 || Animation1)
-                        {
-                            if (Animation1 || Animation2)
-                            {
-                                if (Animation1)
-                                {
-                                    sAnsi = random.Next(1, 11);
-                                }
-                                if (Animation2)
-                                {
-                                    number = random.Next(0, 14);
-                                }
-                                Console.SetCursorPosition(sLeft, sTop);
-                            }
-                            for (int i = 0; i < count; i++)
-                            {
-                                output(text[i], array[number], sAnsi, color);
+                        //The offset essentially converts the character into an integer which is between 1 and 25. It then offsets it with 1 represented with the character 'a'. This is beacuse the text style is repsented in an integer form or in this case an hexadecimal, which you can see in the dictionaries. Each letter in the alphabet in the given text style is represented by the hexadecimal plus the the integer form of the letter, which corresponds to the number in its place in the alphabetic sequence. For example the text style Monospace of the letter m is 0x1D68A + 13, because m is the 13 letter in the alphabet. But 0x1D68A actually is the representation of the letter a, which is the reason why we need the offset of 1, because otherwise the letter a would become the text style b 0x1D68A(the letter a) + 1 = 0x1D68B = letter b. It also includes the other letters.  
+                        int offset = character - 'a';
+                        //The char.ConvertFromUtf32 converts the given calculated hexadecimal into type char. The added content of the string "\x1b + Ansi + "m" + (The character) + \x1b[0m" is the method of converting the text into the corresponding ANSI escape code. In more details the "\1x1b(Ansi)m" is what actually determines the ANSi escape code and has 10 different ANSI types and is represented through 0 to 9. In short the ANSI escape code just tells the terminal of how to display the text" 
+                        Console.Write("\x1b[" + ANSI + "m" + char.ConvertFromUtf32(offset + lowertypeDictionary[TextStyle]) + "\x1b[0m");
+                    }
 
-                            }
-                            output(c, array[number], sAnsi, color);
+                    //This is the else if statement of the uppercases of the english alphabet and works exactly the same as the lowercases, but has different unicode hexadecimals
+                    else if (character >= 'A' && character <= 'Z' && TextStyle != Style.Normal)
+                    {
+                        int offset = character - 'A';
+                        Console.Write("\x1b[" + ANSI + "m" + char.ConvertFromUtf32(offset + uppertypeDictionary[TextStyle]) + "\x1b[0m");
+                    }
 
-                            if (Animation1 || Animation2)
-                            {
-                                count++;
-                            }
-                            if (count == text.Length)
-                            {
-                                stopAnimation = true;
-                                TextList.Add(text);
-                                LeftList.Add(sLeft);
-                                TopList.Add(sTop);
-                                ColorList.Add(color);
-                                Animation1List.Add(Animation1);
-                                Animation2List.Add(Animation2);
-                                AnsiList.Add(sAnsi);
-                                NumberList.Add(number);
-                                if (TextList.Count == 1)
-                                {
-                                    Thread animationThread = new Thread(Animation);
-                                    animationThread.Start();
-                                }
-                            }
-                        }
-                        left = Console.CursorLeft;
-                        top = Console.CursorTop;
-                        thread1 = false;
-                        if (punctDelay != 1)
-                        {
-                            if (c == '.' || c == '!' || c == '?')
-                            {
-                                multiplier = punctDelay;
-                            }
-                        }
-                        Task.Delay(charDelay * multiplier).Wait();
-                        multiplier = 1;
-                        break;
+                    //This else statemenmt only fires if the text style is Normal, because we don't have to through the unicode hexadecimal calculation.
+                    else
+                    {
+                        Console.Write("\x1b[" + ANSI + "m" + character + "\x1b[0m");
                     }
                 }
-                while (true);
-            }
-            Console.Write(enter ? "\n" : " ");
-            left = Console.CursorLeft;
-            top = Console.CursorTop;
-        }
-
-        static void output(char c, Style type, int Ansi, ConsoleColor color)
-        {
-            Console.ForegroundColor = color;
-            if (type != Style.Normal)
-            {
-                if (c >= 'a' && c <= 'z')
-                {
-                    int offset = c - 'a';
-                    Console.Write("\x1b[" + Ansi + "m" + char.ConvertFromUtf32(offset + lowertypeDictionary[type]) + "\x1b[0m");
-                }
-
-                else if (c >= 'A' && c <= 'Z')
-                {
-                    int offset = c - 'A';
-                    Console.Write("\x1b[" + Ansi + "m" + char.ConvertFromUtf32(offset + uppertypeDictionary[type]) + "\x1b[0m");
-                }
-
                 else
                 {
-                    Console.Write(c);
+                    Console.Write("Invalid text style type");
+                    break;
                 }
+
+                //This is the code that gives the delay between each display of each character to the terminal. It consist of a sort of "if statement". If the character is a punctuation mark then have the delay to be the charDelay multiplied by the punctDelay. Otherwise have the delay to be just the charDelay
+                Task.Delay((character == '.' || character == '?' || character == '!') ? (charDelay * punctDelay) : (charDelay)).Wait();
+            }
+
+            //These if and else statement just determines if the next text should be displayed on the same line or on a new 
+            if (newLine)
+            {
+                Console.WriteLine("");
             }
             else
             {
-                Console.Write("\x1b[" + Ansi + "m" + c + "\x1b[0m");
+                Console.Write(" ");
             }
-        }
-
-        static string ReadInput()
-        {
-            string inputBuffer = "";
-            while (true)
-            {
-                if (Console.KeyAvailable)
-                {
-                    thread1 = true;
-                    var key = Console.ReadKey(true);
-                    do
-                    {
-                        if (!thread2)
-                        {
-                            if (key.Key == ConsoleKey.Enter)
-                            {
-                                thread1 = false;
-                                return inputBuffer;
-                            }
-                            else
-                            {
-                                inputBuffer += key.Key;
-                                Console.SetCursorPosition(left, top);
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write(key.KeyChar);
-                                if (left == Console.WindowWidth - 1)
-                                {
-                                    top++;
-                                    left = 0;
-                                }
-                                else
-                                {
-                                    left++;
-                                }
-                                thread1 = false;
-                            }
-                            break;
-                        }
-                    } while (true);
-                }
-            }
-        }
-
-        static void Animation()
-        {
-            do
-            {
-                if (!thread1)
-                {
-                    thread2 = true;
-                    for (int i = 0; i < TextList.Count; i++)
-                    {
-                        int lAnsi = 0;
-                        int lNumber = 0;
-                        if (Animation1List[i])
-                        {
-                            lAnsi = random.Next(1, 11);
-                        }
-                        else if (Animation2List[i])
-                        {
-                            lNumber = random.Next(0, 14);
-                        }
-                        else
-                        {
-                            lAnsi = AnsiList[i];
-                            lNumber = NumberList[i];
-                        }
-                        int lLeft = LeftList[i];
-                        int lTop = TopList[i];
-                        for (int j = 0; j < TextList[i].Length; j++)
-                        {
-                            while (true)
-                            {
-                                Console.SetCursorPosition(lLeft, lTop);
-                                output(TextList[i][j], array[lNumber], lAnsi, ColorList[i]);
-                                lLeft++;
-                                break;
-                            }
-
-                        }
-                    }
-                    Console.SetCursorPosition(left, top);
-                    thread2 = false;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Task.Delay(200).Wait();
-                }
-            }
-            while (stopAnimation);
-            TextList = new List<string>();
-            LeftList = new List<int>();
-            TopList = new List<int>();
-            ColorList = new List<ConsoleColor>();
-            Animation1List = new List<bool>();
-            Animation2List = new List<bool>();
-            AnsiList = new List<int>();
-            NumberList = new List<int>();
         }
     }
 }
