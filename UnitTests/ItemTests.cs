6e80ac6b73b4f3ca6833972ac1a8e6b008414b73
ElminP <elpit25@student.sdu.dk>
2025-11-26 20:20:59 +0100

    Added: ItemTests.cs for Unit Testing

---
+++ UnitTests/ItemTests.cs
@@ -0,0 +1,24 @@
+﻿using cs.Domain.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    internal class ItemTests
+    {
+        private Item mobil;
+        private Item pandekage;
+        private Item toiletNøgle;
+
+        public void Setup()
+        {
+            // Create dummy items (also the items in game) to use for testing
+            mobil = new Item("Mobil", "En smartphone");
+            pandekage = new Item("Pandekage", "MMMM mums en pandekage");
+            toiletNøgle = new Item("Toilet Nøgle", "En nøgle til toilettet");
+        }
+    }
+}
