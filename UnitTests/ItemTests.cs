9b7d09b0782ec8bd4d8cd522bc10bfbc87e21984
ElminP <elpit25@student.sdu.dk>
2025-11-26 21:43:19 +0100

    Created Test 1: Item Constructor - Testing the Constructor for property verification

--- UnitTests/ItemTests.cs
+++ UnitTests/ItemTests.cs
@@ -9,16 +9,35 @@ namespace UnitTests
 {
     internal class ItemTests
     {
+        // Define variables reuse from InventoryTests.cs - Which will be use for multiple tests
         private Item mobil;
         private Item pandekage;
         private Item toiletNøgle;
 
         public void Setup()
         {
-            // Create dummy items (also the items in game) to use for testing
+            // Created dummy items (also the items in actual game) to use for testing
             mobil = new Item("Mobil", "En smartphone");
             pandekage = new Item("Pandekage", "MMMM mums en pandekage");
             toiletNøgle = new Item("Toilet Nøgle", "En nøgle til toilettet");
+        }
+
+        // TEST 1: Constructor
+        // Verifying that the constructor sets all properties correctly
+        public void Item_Constructor_SetsPropertiesCorrectly()
+        {
+            // ACTON: Create a new item
+            Item testItem = new Item("Test Item", "A test description");
+
+            // ASSERT: Verify the results
+            // 1. The name should be set correctly
+            Assert.AreEqual("Test Item", testItem.Name, "Item name should be 'Test Item'.");
+
+            // 2. The description should be set correctly
+            Assert.AreEqual("A test description", testItem.Description, "Item description should be 'A test description'.");
+
+            // 3. The ID should be assigned (not negative)
+            Assert.GreaterOrEqual(testItem.ID, 0, "Item ID should be 0 or greater.");
         }
     }
 }
